aba6bd9d3041ec3588f43d4378c58a63c05ecaf4
Daniel Borges <111613566+danielrockemback@users.noreply.github.com>
2025-01-21 21:47:16 -0300

    2_dataHora.php

--- estudo2_nov2024/2_dataHora.php
+++ estudo2_nov2024/2_dataHora.php
@@ -1,11 +1,13 @@
 <?php
+# Aqui definimos o fuso horário padrão do script para America/Sao_Paulo
 date_default_timezone_set('America/Sao_Paulo');
 
-
+# Pega a hora de agora e formata a data no padrão brasileiro
 $data = new DateTime('now');
-echo $data->format('d/m/Y H:i:s') . PHP_EOL;    # Formatar a data
+echo $data->format('d/m/Y H:i:s') . PHP_EOL;
 
-$data->add(new DateInterval('P10Y5M30D'));     # Adicionar 10 anos(P10Y), 5 meses(5M) e 30 dias (30D)
+ # Adicionar 10 anos(P10Y), 5 meses(5M) e 30 dias (30D) a partir da data escolhida
+$data->add(new DateInterval('P10Y5M30D'));    
 echo $data->format('d/m/Y H:i:s') . PHP_EOL;
 
 echo PHP_EOL;
@@ -19,29 +21,30 @@ echo "Já tem $diferenca->days dias da última compra do cliente, entre em conta
 echo PHP_EOL;
 
 $dataCompra = new DateTime('2024-10-22');
+$dataBaixa = new DateTime('2024-10-28');
 $dataEntrega = new DateTime('2024-10-30');
-
-var_dump($dataCompra > $dataEntrega);
-var_dump($dataCompra < $dataEntrega);
-var_dump($dataCompra == $dataEntrega);
-
-echo PHP_EOL;
 
 $flag = $dataEntrega->diff($dataCompra)->days;
 $faltaQuantosDias = $dataCompra;
 
-echo "A Entrega vai ser feita no dia {$dataEntrega->format('d/m/Y')} entre 9:00 horas até ás 18:00 horas" . PHP_EOL;
+#                            DateInterval Class
+# A classe DateInterval é usada para representar uma diferença entre datas. Ela permite que você adicione ou subtraia períodos de tempo a um objeto DateTime
+
+echo "A Entrega vai ser feita até o dia {$dataEntrega->format('d/m/Y')} entre 9:00 horas até ás 18:00 horas" . PHP_EOL;
 for ($dia = $flag; $dia >= 1; $dia--) {
     $faltaQuantosDias->add(new DateInterval('P1D'));
 
     if ($dia != 1) {
         echo "Data de hoje: {$faltaQuantosDias->format('d/m/Y')} faltam $dia dias para a sua compra ser entregue." . PHP_EOL;
         continue;
-    }else{
-        echo "Data de hoje: {$faltaQuantosDias->format('d/m/Y')} a sua compra vai ser entregue hoje." . PHP_EOL;
     }
+    echo "Data de hoje: {$faltaQuantosDias->format('d/m/Y')} a sua compra vai ser entregue hoje." . PHP_EOL;
+
 };
 
+#                 Comparando Datas com DateTime
+var_dump($dataBaixa > $dataEntrega);    # Entregou no prazo
+var_dump($dataBaixa < $dataEntrega);    # Entrega realizada antes do prazo
 echo PHP_EOL;
 
 # Date Time Constants
